e0227052ab66f3b60308771fe8288beab5a0e3de
Chetan Sonigara <chetansonigara01@gmail.com>
2026-09-20 21:49:21 +0530

    feat: load production odometry parameters

--- launch/lidar_odometry.launch.py
+++ launch/lidar_odometry.launch.py
@@ -6,27 +6,25 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    package_name = 'simple_lidar_odometry'
-
+    package_name = "simple_lidar_odometry"
     package_share = Path(get_package_share_directory(package_name))
-    rviz_config = package_share / 'rviz' / 'lidar.rviz'
+    rviz_config = package_share / "rviz" / "lidar.rviz"
+    params_file = package_share / "config" / "odometry.yaml"
 
     lidar_odometry_node = Node(
         package=package_name,
-        executable='lidar_odometry_node',
-        name='lidar_odometry_node',
-        output='screen',
+        executable="lidar_odometry_node",
+        name="lidar_odometry_node",
+        output="screen",
+        parameters=[str(params_file)],
     )
 
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', str(rviz_config)],
-        output='screen',
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", str(rviz_config)],
+        output="screen",
     )
 
-    return LaunchDescription([
-        lidar_odometry_node,
-        rviz_node,
-    ])
+    return LaunchDescription([lidar_odometry_node, rviz_node])
